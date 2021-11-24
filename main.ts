2cac50a9825f8057f351831d5adfaf02ba373837
Tremse1 <90926884+Tremse1@users.noreply.github.com>
2021-11-24 00:48:43 -0500

    Fichiers initiaux du projet MakeCode

---
+++ main.ts
@@ -0,0 +1,25 @@
+let démarrage = false
+let seconds = 0
+let Pulsations = 0
+input.onButtonPressed(Button.A, function () {
+    démarrage = false
+    seconds += 1
+    basic.showNumber(seconds)
+})
+input.onButtonPressed(Button.B, function () {
+    démarrage = true
+    while (seconds > 0) {
+        basic.pause(1000)
+        seconds += -1
+    }
+    démarrage = false
+})
+basic.forever(function () {
+    if (seconds > 0 && démarrage == true) {
+        Pulsations = pins.analogReadPin(AnalogPin.P2)
+        led.plotBarGraph(
+        Pulsations,
+        1023
+        )
+    }
+})
